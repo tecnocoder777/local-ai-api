30d93c0907df10d49e55fa7302f7a149ad4d76e3
TECNO-CODER <jadejakhanubha73@gmail.com>
2026-01-20 15:52:21 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,17 +1,21 @@
-import brain from "../data.json";
+import fs from "fs";
+import path from "path";
 
 export default function handler(req, res) {
   const msg = (req.query.msg || "").toLowerCase();
 
+  const filePath = path.join(process.cwd(), "data.json");
+  const data = JSON.parse(fs.readFileSync(filePath, "utf8"));
+
   // direct match
-  if (brain[msg]) {
-    return res.send(brain[msg]);
+  if (data[msg]) {
+    return res.send(data[msg]);
   }
 
   // partial match
-  for (let key in brain) {
+  for (let key in data) {
     if (msg.includes(key)) {
-      return res.send(brain[key]);
+      return res.send(data[key]);
     }
   }
 
